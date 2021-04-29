9a31e3f8539bb15f857dbba05eee0b2f1d08a9ad
huitoutunao <lh_huitoutunao@163.com>
2021-04-29 15:42:44 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="page">
         <ul class="home-list">
-            <router-link to="/fixed-table-demo" tag="li" class="item">1、table固定表头和首列</router-link>
-            <router-link to="/fixed-table-demo" tag="li" class="item">2、table固定表头和首列</router-link>
+            <router-link to="/fixed-table-demo" tag="li" class="item">1、table固定表头和首列0</router-link>
+            <router-link to="/fixed-table-demo" tag="li" class="item">2、table固定表头和首列1</router-link>
         </ul>
     </div>
 </template>
